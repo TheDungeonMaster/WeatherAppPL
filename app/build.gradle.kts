803c89e0126865ffea1a9200ddfa551ebf9c62f3
abdi <abdiakhmet.1.618@gmail.com>
2024-05-20 14:13:09 +0500

    fixed gradle issues

---
+++ app/build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+}
+
+android {
+    namespace = "com.example.dagger_hilt_project"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.dagger_hilt_project"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+}
+
+dependencies {
+
+}
